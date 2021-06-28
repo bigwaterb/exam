cea986baf27a880da9d1c6da02a6642230bc61b5
GongJunQun <2900603259@qq.com>
2021-06-28 10:48:11 +0800

    管理员

---
+++ config/prod.env.js
@@ -0,0 +1,7 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  // BASE_API: '"http://maweitao.top/oes/api"',
+  BASE_API: '"/api"',
+  BASE_WEBSOCKET: '"ws://maweitao.top:8080/oes/api/websocket/"'
+}
---
+++ src/api/login.js
@@ -0,0 +1,12 @@
+/*
+  与后台交互模块 （依赖已封装的ajax函数）
+ */
+import ajax from '@/config/ajax'
+// const BASE_URL = process.env.BASE_API + '/admin'
+const BASE_URL = 'http://rjxh.ltd/api/Users'
+
+// 校验管理员登录
+// export const reqLogin = (ano, admPsw) => ajax(BASE_URL + '/adminLogin', { ano, admPsw }, 'POST')
+export const reqLogin = (LoginName, UserPwd) => ajax(BASE_URL + '/PostCheckLogin', { LoginName, UserPwd }, 'POST')
+// 请求退出登录
+export const reqLogOut = () => ajax(BASE_URL + '/adminLogOut')
